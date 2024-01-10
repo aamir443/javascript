19178dc2b3ee19671ee94c6c03bcb48a345b8ce3
Mohammad Aamir <121339457+aamir930@users.noreply.github.com>
2024-01-11 02:35:18 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,9 @@
+const accountId=12345
+let accountEmail="mohammadaamir2002@gmail.com"
+var accountPassword="4556"
+accountcity="varanasi"
+accountstate="UP"
+/* prefered not use var
+because of issue in block scope and function scope*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountstate]);
